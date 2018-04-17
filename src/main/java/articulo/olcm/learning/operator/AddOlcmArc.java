4409265be69027f5173bb3f1d8e2f982ead071cb
ferjorosa <ferjorosa@gmail.com>
2018-04-17 16:48:32 +0200

    OLCM HC operators update

--- src/main/java/articulo/olcm/learning/operator/AddOlcmArc.java
+++ src/main/java/articulo/olcm/learning/operator/AddOlcmArc.java
@@ -1,9 +1,15 @@
 package articulo.olcm.learning.operator;
 
 import voltric.data.DiscreteData;
+import voltric.graph.Edge;
 import voltric.learning.LearningResult;
 import voltric.learning.parameter.em.AbstractEM;
 import voltric.model.DiscreteBayesNet;
+import voltric.model.DiscreteBeliefNode;
+import voltric.variables.DiscreteVariable;
+import voltric.variables.Variable;
+
+import java.util.stream.Collectors;
 
 /**
  * Created by equipo on 16/04/2018.
@@ -11,8 +17,42 @@ import voltric.model.DiscreteBayesNet;
  * Adds a non-repeated arc between a LV and a MV, forming a OLCM
  */
 public class AddOlcmArc implements OlcmHcOperator{
+
     @Override
     public LearningResult<DiscreteBayesNet> apply(DiscreteBayesNet seedNet, DiscreteData data, AbstractEM em) {
-        return null;
+
+        // The BN is copied to avoid modifying current object.
+        DiscreteBayesNet clonedNet = seedNet.clone();
+
+        double bestModelScore = -Double.MAX_VALUE; // Log-likelihood related scores are negative
+        LearningResult<DiscreteBayesNet> bestModelResult = null;
+
+        // Itera por cada variable latente y añade un arco entre ella y cada MV a la cual no tenga como hijo
+        for(DiscreteBeliefNode latentNode : clonedNet.getLatentNodes()){
+            // Filtramos los MVs que no sean hijos de la variable latente
+            for(DiscreteBeliefNode manifestNode: clonedNet.getManifestNodes().stream()
+                    .filter(x->!latentNode.getChildrenNodes().contains(x))
+                    .collect(Collectors.toList())){
+
+                // Añade un arco desde la LV a la MV no-hija
+                Edge<Variable> newEdge = clonedNet.addEdge(manifestNode, latentNode);
+                // Aprende la nueva red con el EM
+                LearningResult<DiscreteBayesNet> newEdgeResult = em.learnModel(clonedNet, data);
+
+                if(newEdgeResult.getScoreValue() > bestModelScore){
+                    bestModelResult = newEdgeResult;
+                    bestModelScore = newEdgeResult.getScoreValue();
+                }
+
+                // Independientemente de si es mejor o no, eliminamos el nuevo arco para dejarlo como la estructura inicial
+                clonedNet.removeEdge(newEdge);
+            }
+        }
+
+        if(bestModelResult != null)
+            return bestModelResult;
+
+        //throw new IllegalStateException("No me creo que no haya ningun modelo que mejore el -double.MAX_VALUE score");
+        return new LearningResult<>(null, bestModelScore, em.getScoreType());
     }
 }
